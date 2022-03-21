b7cedba99511f20adf94f5ecfa87ff313474ef12
Ahmd-Khaled <ahmd.khaldd@gmail.com>
2022-03-21 15:46:45 +0200

    First version of Letsia Project-2

--- js/letsia.js
+++ js/letsia.js
@@ -1,3 +1,20 @@
+// Adding Image title from data set attr
+let box = document.querySelectorAll(".our-works .tabs .tab-content .box");
+let boxAttr;
+let imgTitle;
+
+// console.log(boxAttr);
+box.forEach(function (ele) {
+    console.log(ele)
+    boxAttr = ele.getAttribute("data-imgtitle");
+    imgTitle = document.createTextNode(boxAttr);
+    ele.appendChild(imgTitle);
+});
+
+
+
+
+
 // Scroll to Top
 let toTopBtn = document.querySelector(".to-top");
 
